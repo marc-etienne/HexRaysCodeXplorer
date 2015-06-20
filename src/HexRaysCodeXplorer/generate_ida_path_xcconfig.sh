609cbbdb5d68462234c4b9555609fa98a636d640
Marc-Etienne M.Léveillé <leveille@eset.com>
2015-06-20 17:30:16 -0400

    Add Xcode project files to start working on fixing non-portable stuff

---
+++ src/HexRaysCodeXplorer/generate_ida_path_xcconfig.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+#  generate_ida_path_xcconfig.sh
+#  HexRaysCodeXplorer
+#
+#  Created by Marc-Etienne M.Léveillé on 2015-06-20.
+#  Copyright (c) 2015 Marc-Etienne M.Léveillé
+
+IDA_APP_PATH="$(mdfind "kMDItemCFBundleIdentifier = com.hexrays.idaq" | head -1)"
+IDA_SDK="$(dirname "$(dirname "$(mdfind IDB_EXT | grep '/ida.hpp$')")")"
+
+if [ -z "$IDA_APP_PATH" ]; then
+    echo "Could not determine idaq.app path." > /dev/stderr
+fi
+
+if [ -z "$IDA_SDK" ]; then
+    echo "Could not determine IDA SDK path." > /dev/stderr
+fi
+
+(
+    echo IDA_APP_PATH=\""$IDA_APP_PATH"\"
+    echo IDA_SDK=\""$IDA_SDK"\"
+) > "$SRCROOT/ida_path.xcconfig"
